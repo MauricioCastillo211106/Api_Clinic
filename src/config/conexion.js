1e6005eb27a9fe4a2fd10d89874ec9b8ae4b2a7a
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2023-12-01 11:36:06 -0600

    [ADD] DB deployed

--- src/config/conexion.js
+++ src/config/conexion.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '277353',
-  database: 'Integrador'
+  host: '52.54.125.19',
+  user: 'securide',
+  password: 'password',
+  database: 'integrador'
 });
 connection.connect((err) => {
   if (err) {
